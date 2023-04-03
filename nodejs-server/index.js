d4160f5629212c8175ab7e80d1bef9e58df00bf5
Alan MacDougall <smoke@alanmacdougall.com>
2023-04-03 15:28:22 -0400

    Demo routes for MongoDB access.

--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -1,15 +1,40 @@
 require('dotenv').config();
 // enables process.env.VARNAME
 const mongoose = require('mongoose');
-
 const express = require('express');
+
+const Video = require('./model/video');
+
 const app = express();
+app.use(express.json());
+
 const PORT = process.env.NODE_DOCKER_PORT || 5000;
 
 app.get('/status', (req, res) => {
+  res.send({status: "Hello world."});
+});
+
+app.get('/dbstatus', (req, res) => {
   mongoose.connect("mongodb://root:example@mongodb").then(() => {
     res.send({status: "Hello world. Successfully connected with mongoose. Now what?"});
   });
+});
+
+app.post('/video', async (req, res) => {
+  const { title, url } = req.body; // TODO: null/empty check, etc
+
+  await mongoose.connect("mongodb://root:example@mongodb");
+  const video = await Video.create({ title, url });
+
+  res.send({status: `Posted video ${video}`});
+});
+
+app.get('/videos', async (req, res) => {
+  await mongoose.connect("mongodb://root:example@mongodb");
+  const videos = await Video.find({});
+  // TODO: pagination? no, because we actually aren't going to need this route
+  // at all.
+  res.send({ videos });
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`))
