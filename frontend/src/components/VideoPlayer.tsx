be9d0fb1c3548e48846aa1d2b783f32fb5451551
Alan MacDougall <smoke@alanmacdougall.com>
2023-04-14 16:10:42 -0400

    Use react-youtube to load YouTube video.

--- frontend/src/components/VideoPlayer.tsx
+++ frontend/src/components/VideoPlayer.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-
-// TODO: business logic to handle YouTube video player events
+import YouTube from 'react-youtube';
 
 const VideoPlayer = () => {
+  // has to be included in both style and opts; not sure why
+  const videoDimensions = { width: '100%', height: '100%' };
+
   return (
     <div style={{position: 'relative', margin: 0, paddingBottom: '56.25%', height: 0}}>
-      <iframe
-        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-        src="https://www.youtube.com/embed/xSjcLZ-btfY" title="YouTube video player" 
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen>
-      </iframe>
+      <YouTube
+        videoId="xSjcLZ-btfY"
+        // no frame border
+        style={{
+          ...videoDimensions,
+          position: 'absolute',
+          top: 0,
+          left: 0
+        }}
+        opts={{
+          ...videoDimensions,
+          frameBorder: 0
+        }}
+      />
     </div>
   );
-}
+};
 
 export default VideoPlayer;
