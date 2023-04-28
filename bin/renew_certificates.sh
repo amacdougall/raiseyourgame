4c4ca447037d000ea166b22aba22538e3cd4915b
Alan MacDougall <smoke@alanmacdougall.com>
2023-04-28 10:15:11 -0400

    Update cert renewal script to run from root crontab.

--- bin/renew_certificates.sh
+++ bin/renew_certificates.sh
@@ -1,2 +1,10 @@
-sudo docker compose run --rm certbot renew
-sudo docker compose exec frontend nginx -s reload
+# NOTE: run this from the root crontab, since DigitalOcean docker needs sudo
+# anyway.
+#
+# TODO: make this at least a little bit more portable by providing config file
+# as an argument; but for now, this is only expected to run in one context on
+# one server.
+export CONFIG_FILE=/home/rygadmin/raiseyourgame/docker-compose.yml
+
+sudo docker compose -f $CONFIG_FILE run --rm certbot renew
+sudo docker compose -f $CONFIG_FILE exec frontend nginx -s reload
