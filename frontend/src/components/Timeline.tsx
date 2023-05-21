4068b55b522b63a9d46cf83c2bca8f75daa64794
Alan MacDougall <smoke@alanmacdougall.com>
2023-05-21 11:43:50 -0400

    Handle timeline click by seeking.

--- frontend/src/components/Timeline.tsx
+++ frontend/src/components/Timeline.tsx
@@ -29,13 +29,24 @@ const Timeline = ({time, duration, comments, seek}: TimelineProps) => {
     );
   });
 
+  // Thanks, Github Copilot!
+  // Ironically, one of the suggested completions for the preceding comment was
+  // " (not)". Guess it doesn't meet with universal acclaim.
+  const onClick = (event) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const playbackRatio = x / rect.width; // 0.0 to 1.0
+    const time = duration * playbackRatio;
+    seek(time);
+  };
+
   return (
-    <Box sx={{paddingBottom: '1rem'}}>
-      {/* handle click on LinearProgress with seek */}
+    <Box sx={{paddingBottom: "1rem"}}>
       <LinearProgress
-        sx={{height: 10}}
+        sx={{ height: 10, cursor: "pointer" }}
         variant="determinate"
         value={time / duration * 100}
+        onClick={onClick}
       />
       <Box sx={{position: 'relative', marginTop: '-1px', zIndex: '10'}}>
         {markers}
