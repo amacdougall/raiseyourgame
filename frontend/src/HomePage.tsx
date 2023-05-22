203a9ba26255e28b200fa22c105184de3746a0c2
Alan MacDougall <smoke@alanmacdougall.com>
2023-05-22 11:22:28 -0400

    Add micro-tutorial and screenshots to home page.

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -5,6 +5,9 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
 
@@ -31,19 +34,15 @@ const HomePage = () => {
       margin: 'auto',
       padding: '1rem',
     }}>
-      <Typography variant='h1'>
+      <Typography variant={isMobile ? 'h4' : 'h2'}>
         Raise Your Game
       </Typography>
 
       <Typography variant='body1'>
-        Comment on a YouTube video, or get feedback from others.
+        Comment on a YouTube video, or share the link to get feedback.
       </Typography>
 
-      <Typography variant='body1'>
-        TODO: add a few screenshots showing the process of commenting on a video.
-      </Typography>
-
-      <Typography variant='h2'>
+      <Typography variant='h4'>
         Ready to get started?
       </Typography>
 
@@ -72,6 +71,48 @@ const HomePage = () => {
           <Button variant='contained' type='submit'>Let's Go!</Button>
         </Stack>
       </Form>
+
+      <Typography variant='h4'>
+        How it works:
+      </Typography>
+
+      <Typography variant='body1'>
+        Paste YouTube link, get video page, copy URL, paste on Discord
+        or Reddit or whatever. Read, learn, practice, improve. No login,
+        no tracking. You can delete comments if someone is being uncool.
+        Videos disappear after 30 days.
+      </Typography>
+
+      <ImageList cols={isMobile ? 1 : 2}>
+        <ImageListItem key='tutorial_01'>
+          <img src={isMobile ? '/ryg_tutorial_mobile_01.jpg' : '/ryg_tutorial_01.jpg'} />
+          <ImageListItemBar
+            title='Watch'
+            subtitle='See the action unfold'
+            position='top' />
+        </ImageListItem>
+        <ImageListItem key='tutorial_02'>
+          <img src={isMobile ? '/ryg_tutorial_mobile_02.jpg' : '/ryg_tutorial_02.jpg'} />
+          <ImageListItemBar
+            title='Comment'
+            subtitle='Call out highlights, or offer advice'
+            position='top' />
+        </ImageListItem>
+        <ImageListItem key='tutorial_03'>
+          <img src={isMobile ? '/ryg_tutorial_mobile_03.jpg' : '/ryg_tutorial_03.jpg'} />
+          <ImageListItemBar
+            title='Review'
+            subtitle='Comments appear along with the action'
+            position='top' />
+        </ImageListItem>
+        <ImageListItem key='tutorial_04'>
+          <img src={isMobile ? '/ryg_tutorial_mobile_04.jpg' : '/ryg_tutorial_04.jpg'} />
+          <ImageListItemBar
+            title='Look around'
+            subtitle='Click the timeline or a marker to jump'
+            position='top' />
+        </ImageListItem>
+      </ImageList>
     </Stack>
   );
 };
